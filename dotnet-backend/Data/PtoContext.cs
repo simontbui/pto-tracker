e9b9bce677c023f0085bd6d26b0f784c8ca4efe1
Your Name <you@example.com>
2023-07-25 21:53:52 -0700

    started baseline .net backend with entityframework (not yet functional)

---
+++ dotnet-backend/Data/PtoContext.cs
@@ -0,0 +1,27 @@
+﻿using dotnet_backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_backend.Data
+{
+    public class PtoContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+        public PtoContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+                .HasKey(d => d.DepartmentId);
+        }
+    }
+}
