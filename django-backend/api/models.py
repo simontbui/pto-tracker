8972af1a4bef7be536ceb8ea45d9d89686a42bb8
Your Name <you@example.com>
2023-07-31 22:45:32 -0700

    snake-cased class variables to be consistent with postgres

--- django-backend/api/models.py
+++ django-backend/api/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 class Departments(models.Model):
-    departmentid = models.AutoField(primary_key=True)
-    departmentname = models.CharField(max_length=15)
+    department_id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=15)
 
     class Meta:
         db_table = "departments"
 
 class Employees(models.Model):
-    employeeid = models.AutoField(primary_key=True)
-    firstname = models.CharField(max_length=15)
-    lastname = models.CharField(max_length=15)
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=50)
-    departmentid = models.ForeignKey(Departments, on_delete=models.CASCADE, db_column="departmentid")
+    department_id = models.ForeignKey(Departments, on_delete=models.CASCADE, db_column="department_id")
 
     class Meta:
         db_table = "employees"
 
 class Events(models.Model):
-    eventid = models.AutoField(primary_key=True)
-    employeeid = models.ForeignKey(Employees, db_column="employeeid", on_delete=models.CASCADE)
-    datestart = models.DateField()
-    dateend = models.DateField()
+    event_id = models.AutoField(primary_key=True)
+    employee_id = models.ForeignKey(Employees, db_column="employee_id", on_delete=models.CASCADE)
+    date_start = models.DateField()
+    date_end = models.DateField()
     reason = models.CharField(max_length=250)
 
     class Meta:
