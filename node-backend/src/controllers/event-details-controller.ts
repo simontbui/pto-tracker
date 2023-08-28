03b2351429b83769f1b6cc5748ff2d5a10e87288
Your Name <you@example.com>
2023-08-27 23:03:23 -0700

    feature-28: node event-detail filtering through database

--- node-backend/src/controllers/event-details-controller.ts
+++ node-backend/src/controllers/event-details-controller.ts
@@ -1,9 +1,37 @@
 import { Request, Response } from "express";
 import { pool } from "../db-pool/pool";
 import { IEventDetails } from "../models/event-details.interface";
+import _ from "lodash";
 
 export const getEventDetails = (req: Request, res: Response) => {
-    pool.query("SELECT * FROM view_event_details ORDER BY first_name, last_name", (error, results) => {
+    const filters = req.query;
+
+    let query = "SELECT * FROM view_event_details" //ORDER BY first_name, last_name"
+
+    if (!_.isEmpty(filters)) {        
+        query += " WHERE 1=1"
+
+        for (const [key, value] of Object.entries(filters)) {
+            switch(key.toLowerCase()) {
+                case "firstname":
+                case "first_name":
+                    query += ` AND first_name = '${value}'`;
+                    break;
+                case "lastname":
+                case "last_name":
+                    query += ` AND last_name = '${value}'`;
+                    break;
+                case "departmentname":
+                case "department_name":
+                    query += ` AND department_name = '${value}'`;
+                    break;
+            }
+        }
+    }
+
+    query += " ORDER BY first_name, last_name;"
+    
+    pool.query(query, (error, results) => {
         if (error) {
             throw error;
         }
